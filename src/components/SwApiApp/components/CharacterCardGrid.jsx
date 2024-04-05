fc106238596307e07bc3ac91edc3262308bce10b
Sean Atom <hi@logchecks>
2024-04-05 15:41:11 -0400

    fix minor bug

--- src/components/SwApiApp/components/CharacterCardGrid.jsx
+++ src/components/SwApiApp/components/CharacterCardGrid.jsx
@@ -19,12 +19,12 @@ export const CharacterCardGrid = () => {
     <>
       <ActiveCardGrid/>
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {characterList.map((character, index) => index > 2?
+        {characterList.map((character, index) =>
           <Grid item xs={2} sm={4} md={4} key={index}>
             {/*<Item>*/}
               <CharacterCard character={character} />
             {/*</Item>*/}
-          </Grid>: null
+          </Grid>
         )})
       </Grid>
       </>

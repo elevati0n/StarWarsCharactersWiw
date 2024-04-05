66cefe9b9621d8122d7f4e6b69ae876a2e36dac2
Sean Atom <hi@logchecks>
2024-04-05 16:21:47 -0400

    style tweaks code clean up

--- src/components/SwApiApp/components/CharacterCardGrid.jsx
+++ src/components/SwApiApp/components/CharacterCardGrid.jsx
@@ -5,6 +5,8 @@ import { CharacterCard } from "./CharacterCard/CharacterCard"
 import { Grid } from "@mui/material"
 import { ActiveCardGrid } from "./ActiveCardGrid/ActiveCardGrid"
 import { useEffect } from "react"
+import Paper from '@mui/material/Paper'
+import { styled } from '@mui/material/styles'
 
 export const CharacterCardGrid = () => {
   const { useCharacterList } = useSwapiContext()
@@ -15,15 +17,22 @@ export const CharacterCardGrid = () => {
     window.characterList = characterList
   },[characterList])
 
+  const Item = styled(Paper)(({ theme }) => ({
+    // backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    zIndex:3
+  }));
+
   return (
     <>
       <ActiveCardGrid/>
       <Grid container spacing={{ xs: 0, md: 0 }} columns={{ xs: 16, sm: 16, md: 16 }}>
         {characterList.map((character, index) =>
           <Grid item xs={16} sm={12} md={8} lg={6} key={index}>
-            {/*<Item>*/}
               <CharacterCard character={character} />
-            {/*</Item>*/}
           </Grid>
         )})
       </Grid>

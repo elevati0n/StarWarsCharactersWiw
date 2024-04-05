5a84b5757cb43649f02c2f0edcbe7174d2706c3b
Sean Atom <hi@logchecks>
2024-04-05 01:55:34 -0400

    working scroll

--- src/components/SwApiApp/components/CharacterCardGrid.js
+++
@@ -1,8 +0,0 @@
-import * as React from "react"
-import { useSwapiContext } from "../hooks/useSwapiContext"
-
-export const CharacterCardGrid = () => {
-  const { useCharacterList } = useSwapiContext()
-  const characterList = useCharacterList()
-  return <div>{JSON.stringify(characterList) ?? "No Data"}</div>
-}

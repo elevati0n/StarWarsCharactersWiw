b38dae7037186ede9b31057a2698c1f19a7c943d
Sean Atom <hi@logchecks>
2024-04-06 21:52:39 -0400

    remove bad module

--- src/SwApiApp/SwApiApp.tsx
+++ src/SwApiApp/SwApiApp.tsx
@@ -1,21 +1,16 @@
 // @ts-ignore
 import React, { createContext, useContext } from "react"
-// @ts-ignore
-import TagManager from 'react-gtm-module'
 
 import SWCatalog from "./dataStructures/SWCatalog"
 // @ts-ignore
 import { ActiveCardGrid, CharacterCardGrid, IntroCrawl } from "./components"
 
 export const SwApiContext = createContext(new SWCatalog())
-const tagManagerArgs = {
-  gtmId: 'G-BK0336BHEG'
-}
+
 
 // @ts-ignore
 export const SwApiApp = ({children}) => {
   // we need to store the data locally because it requires multiple calls.
-  TagManager.initialize(tagManagerArgs)
   const swCatalog = useContext(SwApiContext)
 
   return (
